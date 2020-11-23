633fee7b030999f4d35a0cc06689da4a22e4cf56
vshaverdov <vlaaad.sh@mail.ru>
2020-11-23 17:39:33 +0300

    Res2

---
+++ Project2/app.py
@@ -0,0 +1,22 @@
+import time
+
+
+slide1 = '(-_-)'
+slide2 = '(~_~)'
+slide3 = '(o_-)'
+slide4 = '(o_O)'
+slide5 = '(o_o)'
+slide6 = 'Привет, Мир! 4'
+
+time.sleep(1)
+print(slide1, end='\r')
+time.sleep(1)
+print(slide2, end='\r')
+time.sleep(1)
+print(slide3, end='\r')
+time.sleep(1)
+print(slide4, end='\r')
+time.sleep(1)
+print(slide5, end='\r')
+time.sleep(1)
+print(slide6)
---
+++ module2/app.py
@@ -0,0 +1,13 @@
+ 
+slide3 = '(I_!)'
+slide4 = '(I_@)'
+slide5 = '(I_#)'
+slide6 = 'Hello, World!'
+
+
+print(slide1)
+print(slide2)
+print(slide3)
+print(slide4)
+print(slide5)
+print(slide6)
